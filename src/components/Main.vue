edfeb8c93f9e85c9f635ab79a5109d93911ce092
kentavros <assh4884@gmail.com>
2017-10-13 14:40:07 +0300

    1

---
+++ src/components/Main.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="main">
+      {{msg}}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'Main',
+  data () {
+    return {
+      msg: 'Welcome Sanek!',
+      url: {
+        home:'http://bookshop1/user6/bookShop/client/api/',
+        class: 'http://192.168.0.15/~user6/bookShop/client/api/'
+      },
+      location: 'home'
+    }
+  },
+  methods: {
+    getAllBooks: function(){
+      var self = this
+          axios.get(self.requestUrl + 'books/')
+            .then(function (response) {
+            console.log(response.data);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    },
+  },
+  computed: {
+      requestUrl(){
+      var self = this
+      if(self.location == 'home'){
+        return self.url.home
+      }
+      else
+      {
+        return self.url.class
+      }
+    }
+  },
+  created(){
+    this.getAllBooks()
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
