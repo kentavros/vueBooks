4e34020f70f5598f99cdb858c5c2804c6a88a372
kentavros <assh4884@gmail.com>
2017-10-15 10:09:06 +0300

    3

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div id="app">
-    
     <router-view/>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app',
+  data(){
+    return {
+      url: {
+        home:'http://bookshop1/user6/bookShop/client/api/',
+        class: 'http://192.168.0.15/~user6/bookShop/client/api/'
+      },
+      location: 'home'
+    }
+  },
+  computed: {
+    getUrl(){
+      var self = this
+      if(self.location == 'home'){
+        return self.url.home
+      }
+      else
+      {
+        return self.url.class
+      }
+    }
+  }
 }
 </script>
 
 <style>
 #app {
+  width: 1000px;
+  margin: auto;
   /* font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px; */
+  color: #2c3e50; */
+  margin-top: 30px;
 }
 </style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,64 @@
 <template>
   <div class="main">
+      <p class="alert-danger">{{errMsg}}</p>
       {{msg}}
+    <div class="row">
+      <div class="col-md-4">
+        //todo:: Login form
+      </div>
+    </div>
+      
+    <div class="row">
+      <div class="sidebar col-md-3">
+        <p>
+        <button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#booksList" aria-expanded="false" aria-controls="collapseExample">
+          Books</button>
+        </p>
+        <div class="collapse" id="booksList">
+            <ul v-for="book in books" class="nav nav-pills nav-stacked">
+              <li ><a href="#">{{book.title}}</a></li>
+            </ul>
+        </div>
+
+        <p>
+        <button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#authorsList" aria-expanded="false" aria-controls="collapseExample">
+          Authors</button>
+        </p>
+        <div class="collapse" id="authorsList">
+            <ul v-for="author in authors" class="nav nav-pills nav-stacked">
+              <li ><a href="#">{{author.name}}</a></li>
+            </ul>
+        </div>
+
+        <p>
+        <button class="btn btn-primary btn-block" type="button" data-toggle="collapse" data-target="#genresList" aria-expanded="false" aria-controls="collapseExample">
+          Genres</button>
+        </p>
+        <div class="collapse" id="genresList">
+            <ul v-for="genre in genres" class="nav nav-pills nav-stacked">
+              <li ><a href="#">{{genre.name}}</a></li>
+            </ul>
+        </div>
+
+      </div>
+      <div class="col-md-9">
+        <div v-for="book in books" class="oneBook col-md-4">
+          <p class="title">{{book.title}}</p>
+          <div class="image">
+            <img :src="book.img" class="img"  />
+          </div>
+          <p class="genre">Genres: </p>
+          <p v-for="genre in book.genres" class="genre">{{genre.name}}/ </p>
+          <br>
+          <p class="author">Authors: </p>
+          <p v-for="author in book.authors" class="author">{{author.name}}/ </p>
+          <p>Discount: {{book.discount}}%</p>
+          <p>Price: {{book.price}}$</p>
+        </div>
+
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -10,47 +68,108 @@ export default {
   name: 'Main',
   data () {
     return {
-      msg: 'Welcome Sanek!',
-      url: {
-        home:'http://bookshop1/user6/bookShop/client/api/',
-        class: 'http://192.168.0.15/~user6/bookShop/client/api/'
-      },
-      location: 'home', 
-      books: []
+      msg: 'Welcome Sanek! 8 days left, hurry!',
+      books: [],
+      authors: [],
+      genres: [],
+      errMsg: ''
     }
   },
   methods: {
-    getAllBooks: function(){
+    getBooks: function(){
       var self = this
-          axios.get(self.requestUrl + 'books/')
+          axios.get(self.$parent.getUrl + 'books/')
             .then(function (response) {
-            console.log(response.data)
-            self.books = response.data
+            // console.log(response.data)
+            if (Array.isArray(response.data))
+            {
+              self.books = response.data
+            }
+            else{
+              self.errMsg = response.data
+            }
         })
         .catch(function (error) {
           console.log(error)
         });
     },
-  },
-  computed: {
-      requestUrl(){
+    getAuthors: function(){
       var self = this
-      if(self.location == 'home'){
-        return self.url.home
-      }
-      else
-      {
-        return self.url.class
-      }
+      axios.get(self.$parent.getUrl + 'authors/')
+            .then(function (response) {
+            // console.log(response.data)
+            if (Array.isArray(response.data))
+            {
+              self.authors = response.data
+            }
+            else{
+              self.errMsg = response.data
+            }
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
+    },
+    getGenres: function(){
+      var self = this
+      axios.get(self.$parent.getUrl + 'genres/')
+            .then(function (response) {
+            // console.log(response.data)
+            if (Array.isArray(response.data))
+            {
+              self.genres = response.data
+            }
+            else{
+              self.errMsg = response.data
+            }
+        })
+        .catch(function (error) {
+          console.log(error)
+        });
     }
   },
+  computed: {
+  },
   created(){
-    this.getAllBooks()
+    this.getBooks()
+    this.getAuthors()
+    this.getGenres()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.sidebar{
+    background-color: lavender;
+    padding: inherit;
+    padding-top: 10px;
+}
+.oneBook{
+  text-align: center;
+  width: 247px;
+  height: 400px;
+  overflow: hidden;
+  margin-bottom: 15px;
+}
+.oneBook p{
+  margin: 0 0 0px;
+}
+.title{
+  font-size: 16px;
+  font-weight: bold;
+  color: darkblue;
+}
+.image{
+ height: 260px;
+}
+.img{
+  width: 100%
+}
+.genre {
+  display: inline;
+}
+.author{
+  display: inline;
+}
 </style>
